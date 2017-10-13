2495c4e8a095d56c962d7db18b0ed44f47847ad2
Pablo Pineda <pablo12.izq@gmail.com>
2017-10-12 18:45:14 -0600

    completed method decrypt

--- AES/Program.cs
+++ AES/Program.cs
@@ -10,7 +10,10 @@ namespace AES
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(encrypt("hola", "hola", 256));
+            string encriptado = encrypt("hola", "hola", 256);
+            string decriptado = decrypt(encriptado, "hola", 256);
+            Console.WriteLine(encriptado);
+            Console.WriteLine(decriptado);
             Console.ReadLine();
         }
 
@@ -77,11 +80,91 @@ namespace AES
                 }
                 ciphertext = ciphertext + string.Join("",cipherChar);
             }
+            Console.WriteLine(ctrTxt + ciphertext);
             ciphertext = Base64Encode( ctrTxt + ciphertext);
+            
 
             return ciphertext;
         }
 
+        /**
+     * Decrypt a text encrypted by AES in counter mode of operation
+     *
+     * @param   {string} ciphertext - Cipher text to be decrypted.
+     * @param   {string} password - Password to use to generate a key for decryption.
+     * @param   {number} nBits - Number of bits to be used in the key; 128 / 192 / 256.
+     * @returns {string} Decrypted text
+     *
+     * @example
+     *   const decr = AesCtr.decrypt('lwGl66VVwVObKIr6of8HVqJr', 'pāşšŵōřđ', 256); // 'big secret'
+     */
+        static string decrypt(string ciphertext,string password,int nBits)
+        {
+            const int blockSize = 16;  // block size fixed at 16 bytes / 128 bits (Nb=4) for AES
+            if (!(nBits == 128 || nBits == 192 || nBits == 256))
+            {
+                Console.WriteLine("Key size is not 128 / 192 / 256");
+            }
+            ciphertext = Base64Decode(ciphertext);
+            Console.WriteLine(ciphertext);
+            // use AES to encrypt password (mirroring encrypt routine)
+            int nBytes = nBits / 8;  // no bytes in key
+            byte[] pwBytes = new byte[(nBytes)];
+            for (int i = 0; i < nBytes; i++)
+            {  // use 1st nBytes chars of password for key
+                pwBytes[i] = i < password.Length ? Convert.ToByte(password[(i)]) : Convert.ToByte(0);
+            }
+
+            Aes aes = new Aes();
+            byte[] key = aes.cipher(pwBytes, aes.keyExpansion(pwBytes));
+            key = key.Concat(key.Slice(0, nBytes - 16)).ToArray();  // expand key to 16/24/32 bytes long
+
+            // recover nonce from 1st 8 bytes of ciphertext
+            byte[] counterBlock = new byte[10000];
+            string ctrTxt = ciphertext.Slice(0, 8);
+            for (int i = 0; i < 8; i++) counterBlock[i] = Convert.ToByte(ctrTxt[(i)]);
+
+            // generate key schedule
+            byte[,] keySchedule = aes.keyExpansion(key);
+
+            // separate ciphertext into blocks (skipping past initial 8 bytes)
+            double nBlocks = Math.Ceiling((double)(ciphertext.Length - 8) / blockSize);
+            string[] ct = new string[((int)nBlocks)];
+            for (int b = 0; b < nBlocks; b++) ct[b] = ciphertext.Slice(8 + b * blockSize, 8 + b * blockSize + blockSize);
+            string[] ciphertext1 = ct;  // ciphertext is now array of block-length strings
+
+            // plaintext will get generated block-by-block into array of block-length strings
+            string plaintext = "";
+
+            for (int b = 0; b < nBlocks; b++)
+            {
+                // set counter (block #) in last 8 bytes of counter block (leaving nonce in 1st 8 bytes)
+                for (int c = 0; c < 4; c++)
+                {
+                    byte byte1 = Convert.ToByte(((uint)(b) >> c * 8) & 0xff);
+                    counterBlock[15 - c] = Convert.ToByte(((uint)(b) >> c * 8) & 0xff);
+                }
+                for (int c = 0; c < 4; c++)
+                {
+                    double byte1 = ( (double)(b + 1) / 0x100000000 - 1);
+                    byte byte2 = Convert.ToByte(((uint)byte1 >> c * 8) & 0xff);
+                    counterBlock[15 - c - 4] = byte2;
+                }
+
+                byte[] cipherCntr = aes.cipher(counterBlock, keySchedule);  // encrypt counter block
+
+                char[] plaintxtByte = new char[(ciphertext1[b].Length)];
+                for (int i = 0; i < ciphertext1[b].Length; i++)
+                {
+                    // -- xor plaintext with ciphered counter byte-by-byte --
+                    plaintxtByte[i] = Convert.ToChar(cipherCntr[i] ^ Convert.ToByte( ciphertext1[b][(i)]));
+                }
+                plaintext = plaintext + string.Join("",plaintxtByte);
+                
+            }
+            return plaintext;
+        }
+        
         public static string Base64Encode(string plainText)
         {
             var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
@@ -93,6 +176,7 @@ namespace AES
             var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
             return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
         }
+        
     }
 
     public static class Extensions
@@ -118,5 +202,21 @@ namespace AES
             }
             return res;
         }
+
+        /// <summary>
+        /// Get the string slice between the two indexes.
+        /// Inclusive for start index, exclusive for end index.
+        /// </summary>
+        public static string Slice(this string source, int start, int end)
+        {
+            if (end < 0) // Keep this for negative end support
+            {
+                end = source.Length + end;
+            }
+            if (end > source.Length) end = source.Length;
+            int len = end - start;               // Calculate length
+            return source.Substring(start, len); // Return Substring of length
+        }
     }
+    
 }
